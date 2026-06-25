13e61dee504144aec92aae6bb094fa59c2a94c41
johnwolf222 <johnwolfvision@gmail.com>
2026-06-25 14:10:05 -0400

    Update booking.tsx

--- src/routes/booking.tsx
+++ src/routes/booking.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { type TouchEvent, useMemo, useState } from "react";
 import { toast } from "sonner";
-import {
   ArrowLeft,
   ArrowRight,
   CalendarDays,
